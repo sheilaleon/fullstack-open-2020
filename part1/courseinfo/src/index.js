84cb93086ea5a96fb3ca8a2b1b51ccb4495f9394
Sheila Leon <sheilaleon.au@gmail.com>
2020-11-23 20:08:14 +1100

    Exercise 1.5
    Combine course and parts into one variable

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -36,27 +36,29 @@ const Part = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Footer total={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Footer total={course.parts} />
     </div>
   );
 };
