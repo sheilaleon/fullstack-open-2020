6bd5e11f24f0d1b39c5527d1737250a3aeab0d36
Sheila Leon <sheilaleon.au@gmail.com>
2020-11-24 12:26:08 +1100

    1.7: unicafe step2
    Show total number of feedback, average score, and percentage of positive feedback.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const App = () => {
   // save clicks of each button to its own state
@@ -7,7 +8,75 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  let total = good + neutral + bad;
+  let average = ((good * 1 + bad * -1) / total || 0).toFixed(1);
+  let positive = ((good / total) * 100 || 0).toFixed(1);
+
+  const handleClick = (props) => {
+    console.log(props);
+    switch (props) {
+      case 'Good':
+        setGood(good + 1);
+        break;
+      case 'Neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'Bad':
+        setBad(bad + 1);
+        break;
+      default:
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>📣 Give Feedback</h1>
+      <div className="feedback">
+        <button type="button" onClick={() => handleClick('Good')}>
+          <div className="emoji">🥰</div>
+          Good
+        </button>
+        <button type="button" onClick={() => handleClick('Neutral')}>
+          <div className="emoji">😐</div>
+          Neutral
+        </button>
+        <button type="button" onClick={() => handleClick('Bad')}>
+          <div className="emoji">🤢</div>
+          Bad
+        </button>
+      </div>
+      <div className="statistics">
+        <h3>Statistics</h3>
+        <div className="stats-flex">
+          <ul>
+            <li>
+              <span className="label">Good:</span>{' '}
+              <span className="label">{good}</span>
+            </li>
+            <li>
+              <span className="label">Neutral:</span>{' '}
+              <span className="label">{neutral}</span>
+            </li>
+            <li>
+              <span className="label">Bad:</span>{' '}
+              <span className="label">{bad}</span>
+            </li>
+            <li>
+              <span className="label">All:</span>{' '}
+              <span className="label">{total}</span>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <span className="label">Average:</span>{' '}
+              <span className="label">{average}</span>
+            </li>
+            <li>{positive}% Positive</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
