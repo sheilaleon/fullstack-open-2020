379849f60d56f398779a85f21f3ad79d16221f5d
Sheila Leon <sheilaleon.au@gmail.com>
2020-11-24 12:31:21 +1100

    1.8: unicafe step3
    Refactor Statistics section into a separate component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,15 +2,52 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+
+  let total = good + neutral + bad;
+  let average = ((good * 1 + bad * -1) / total || 0).toFixed(1);
+  let positive = ((good / total) * 100 || 0).toFixed(1);
+
+  return (
+    <div className="statistics">
+      <h3>Statistics</h3>
+      <div className="stats-flex">
+        <ul>
+          <li>
+            <span className="label">Good:</span>{' '}
+            <span className="label">{good}</span>
+          </li>
+          <li>
+            <span className="label">Neutral:</span>{' '}
+            <span className="label">{neutral}</span>
+          </li>
+          <li>
+            <span className="label">Bad:</span>{' '}
+            <span className="label">{bad}</span>
+          </li>
+          <li>
+            <span className="label">All:</span>{' '}
+            <span className="label">{total}</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <span className="label">Average:</span>{' '}
+            <span className="label">{average}</span>
+          </li>
+          <li>{positive}% Positive</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  let total = good + neutral + bad;
-  let average = ((good * 1 + bad * -1) / total || 0).toFixed(1);
-  let positive = ((good / total) * 100 || 0).toFixed(1);
 
   const handleClick = (props) => {
     console.log(props);
@@ -45,36 +82,7 @@ const App = () => {
           Bad
         </button>
       </div>
-      <div className="statistics">
-        <h3>Statistics</h3>
-        <div className="stats-flex">
-          <ul>
-            <li>
-              <span className="label">Good:</span>{' '}
-              <span className="label">{good}</span>
-            </li>
-            <li>
-              <span className="label">Neutral:</span>{' '}
-              <span className="label">{neutral}</span>
-            </li>
-            <li>
-              <span className="label">Bad:</span>{' '}
-              <span className="label">{bad}</span>
-            </li>
-            <li>
-              <span className="label">All:</span>{' '}
-              <span className="label">{total}</span>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <span className="label">Average:</span>{' '}
-              <span className="label">{average}</span>
-            </li>
-            <li>{positive}% Positive</li>
-          </ul>
-        </div>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
