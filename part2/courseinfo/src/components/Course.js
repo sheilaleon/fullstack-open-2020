98b9ec2e6429701ad11a7850071238f30adfeac1
Sheila Leon <sheilaleon.au@gmail.com>
2020-11-25 23:30:57 +1100

    2.4: Course information step9
    Allow arbitrary number of courses to be displayed

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Header = ({ title }) => {
-  return <h1>{title}</h1>;
+const Title = ({ title }) => {
+  return <h2>{title}</h2>;
 };
 
 const Part = ({ id, name, exercises }) => {
@@ -22,24 +22,27 @@ const Content = ({ parts }) => {
   );
 };
 
-const Footer = ({ parts }) => {
+const Total = ({ parts }) => {
   const total = parts.reduce(function (acc, obj) {
     return acc + obj.exercises;
   }, 0);
   return (
     <div>
-      Total of <strong>{total}</strong> exercises
+      <strong>Total of {total} exercises</strong>
     </div>
   );
 };
 
-const Course = ({ course }) => {
-  const { id, name, parts } = course;
+const Course = ({ courses }) => {
   return (
-    <div key={id}>
-      <Header title={name} />
-      <Content parts={parts} />
-      <Footer parts={parts} />
+    <div>
+      {courses.map((course) => (
+        <div key={course.id} className="course">
+          <Title title={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
     </div>
   );
 };
