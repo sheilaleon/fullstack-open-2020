0af2b342686dd47e1fd8a20504021e4eb1d41634
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-11 15:03:21 +1100

    2.14*: Data for countries, step3
    Add weather info on Country details view

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Weather = ({ capital }) => {
+  const [currentTemp, setCurrentTemp] = useState([]);
+
+  const apiKey = process.env.REACT_APP_WEATHERSTACK_API;
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${apiKey}&query=${capital}&m&en`,
+      )
+      .then((response) => setCurrentTemp(response.data.current));
+  }, [apiKey, capital]);
+
+  return (
+    <div>
+      <h2>Weather in {capital}</h2>
+      <p>
+        <strong>Temperature:</strong> {currentTemp.temperature}° C
+      </p>
+      <img
+        src={currentTemp.weather_icons}
+        alt={`${currentTemp.temperature}° - ${currentTemp.weather_descriptions}`}
+      />
+      <p>
+        <strong>Wind:</strong> {currentTemp.wind_speed} kph, direction{' '}
+        {currentTemp.wind_dir}
+      </p>
+    </div>
+  );
+};
 
 const CountryDetails = (props) => {
   const { name, capital, population, languages, flag } = props.country;
+
   return (
     <div>
       <h1>{name}</h1>
@@ -18,6 +51,7 @@ const CountryDetails = (props) => {
         ))}
       </ul>
       <img src={flag} alt={name} width="250" style={{ marginTop: '1rem' }} />
+      <Weather capital={capital} />
     </div>
   );
 };
