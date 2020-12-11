114f3b4401e572e95730701f024264c007743500
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-11 20:13:53 +1100

    3.2: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length}.</p>
+    <p>${new Date().toString()}</p>`,
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
