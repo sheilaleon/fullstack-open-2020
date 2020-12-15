e86ae86d123887c5277c1a5296d8dd80d7524bbc
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-15 20:08:24 +1100

    3.16: Phonebook database, step4
    Move the error handling of the application to a new error handler middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,21 +25,22 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 );
 
-const generateId = () => {
-  const id = Math.floor(Math.random() * Math.floor(100000));
-  return id;
-};
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons.map((person) => person.toJSON()));
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -48,10 +49,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
-  // const id = Number(request.params.id);
-  // persons = persons.filter((person) => person.id !== id);
-
-  // response.status(204).end();
 });
 
 app.post('/api/persons', (request, response) => {
@@ -84,6 +81,24 @@ app.get('/info', (request, response) => {
     <p>${new Date().toString()}</p>`,
   );
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
