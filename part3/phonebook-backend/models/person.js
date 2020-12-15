d11caa07d64707585b7a99e111d9b08df1a112ba
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-15 21:38:09 +1100

    3.22: Lint configuration
    Add eslint, fix lint errors, update /info to use database

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 // mongoose definitions
-const url = process.env.MONGODB_URI;
-console.log(url);
-console.log(`Connecting to ${url}...`);
+const url = process.env.MONGODB_URI
+console.log(url)
+console.log(`Connecting to ${url}...`)
 
 mongoose
   .connect(url, {
@@ -13,12 +13,12 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true,
   })
-  .then((result) => {
-    console.log('Connected to MongoDB.');
+  .then(() => {
+    console.log('Connected to MongoDB.')
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message);
-  });
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -32,16 +32,16 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     required: true,
   },
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
