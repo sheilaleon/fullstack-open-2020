827f6fd2b25285a711225179b4531719fe5909aa
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-16 17:34:09 +1100

    4.5*: helper functions and unit tests, step3
    Tests for Favourite Blogs

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,4 +1,55 @@
+
+
 const listHelper = require('../utils/list_helper')
+
+const emptyList = [
+]
+const listWithOnlyOneBlog = [
+  {
+    'title': 'Understanding Modules, Import and Export in JavaScript',
+    'author': 'Tania Rascia',
+    'url': 'https://www.taniarascia.com/javascript-modules-import-export/',
+    'likes': 12,
+    'id': '5fd94ecd788b3820307cc0af'
+  }
+]
+const blogs = [
+  {
+    'title': 'Understanding Modules, Import and Export in JavaScript',
+    'author': 'Tania Rascia',
+    'url': 'https://www.taniarascia.com/javascript-modules-import-export/',
+    'likes': 1,
+    'id': '5fd94ecd788b3820307cc0af'
+  },
+  {
+    'title': 'How To Use the JavaScript Developer Console',
+    'author': 'Lisa Tagliaferri',
+    'url': 'https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-developer-console',
+    'likes': 1,
+    'id': '5fd94f0c788b3820307cc0b0'
+  },
+  {
+    'title': 'JavaScript to Know for React',
+    'author': 'Kent C. Dodds',
+    'url': 'https://kentcdodds.com/blog/javascript-to-know-for-react',
+    'likes': 1,
+    'id': '5fd94f5a788b3820307cc0b1'
+  },
+  {
+    'title': 'React Tutorial: An Overview and Walkthrough',
+    'author': 'Tania Rascia',
+    'url': 'https://www.taniarascia.com/getting-started-with-react/',
+    'likes': 3,
+    'id': '5fd9a885c78a79990d49d946'
+  },
+  {
+    'title': 'Design for Developers: Specific Steps to Improve Your Website Design',
+    'author': 'Tania Rascia',
+    'url': 'https://www.taniarascia.com/design-for-developers/',
+    'likes': 1,
+    'id': '5fd9a8adc78a79990d49d947'
+  }
+]
 
 test('dummy returns 1', () => {
   const blogs = []
@@ -8,41 +59,6 @@ test('dummy returns 1', () => {
 })
 
 describe('total likes', () => {
-  const emptyList = [
-  ]
-  const listWithOnlyOneBlog = [
-    {
-      'title': 'Understanding Modules, Import and Export in JavaScript',
-      'author': 'Tania Rascia',
-      'url': 'https://www.taniarascia.com/javascript-modules-import-export/',
-      'likes': 12,
-      'id': '5fd94ecd788b3820307cc0af'
-    }
-  ]
-  const listWithSeveralBlogs = [
-    {
-      'title': 'Understanding Modules, Import and Export in JavaScript',
-      'author': 'Tania Rascia',
-      'url': 'https://www.taniarascia.com/javascript-modules-import-export/',
-      'likes': 12,
-      'id': '5fd94ecd788b3820307cc0af'
-    },
-    {
-      'title': 'How To Use the JavaScript Developer Console',
-      'author': 'Lisa Tagliaferri',
-      'url': 'https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-developer-console',
-      'likes': 6,
-      'id': '5fd94f0c788b3820307cc0b0'
-    },
-    {
-      'title': 'JavaScript to Know for React',
-      'author': 'Kent C. Dodds',
-      'url': 'https://kentcdodds.com/blog/javascript-to-know-for-react',
-      'likes': 4,
-      'id': '5fd94f5a788b3820307cc0b1'
-    }
-  ]
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes(emptyList)
     expect(result).toBe(0)
@@ -53,7 +69,31 @@ describe('total likes', () => {
   })
 
   test('of a bigger list is calculated correctly', () => {
-    const result = listHelper.totalLikes(listWithSeveralBlogs)
-    expect(result).toBe(22)
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(7)
+  })
+})
+
+describe('favourite blogs', () => {
+  test('when list has no blogs', () => {
+    const result = listHelper.favouriteBlogs(emptyList)
+    expect(result).toBe(null)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.favouriteBlogs(listWithOnlyOneBlog)
+    expect(result).toEqual({
+      'author': 'Tania Rascia', 'likes': 12,
+      'title': 'Understanding Modules, Import and Export in JavaScript'
+    })
+  })
+
+  test('when list has multiple blogs equals the most liked blog of that', () => {
+    const result = listHelper.favouriteBlogs(blogs)
+    expect(result).toEqual({
+      'author': 'Tania Rascia',
+      'likes': 3,
+      'title': 'React Tutorial: An Overview and Walkthrough',
+    })
   })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -4,14 +4,32 @@ const dummy = () => {
 // or
 // const dummy = (blogs) => 1
 
-const totalLikes = (listWithOneBlog) => {
-  const likes = listWithOneBlog.reduce((total,  value) => {
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((total,  value) => {
     return total + value.likes
   }, 0)
   return likes
 }
 
+const favouriteBlogs = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return null
+  }
+
+  // find highest voted blog
+  const highestVoted = blogs.reduce((previous, current) => previous.likes > current.likes ? previous : current)
+
+  const fav = {
+    title: highestVoted.title,
+    author: highestVoted.author,
+    likes: highestVoted.likes
+  }
+
+  return fav
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlogs
 }
