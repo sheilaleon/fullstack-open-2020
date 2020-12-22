48985f0ff87f783a4bf4853bd759001f116e3bc9
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-22 18:31:34 +1100

    5.7* Blog list frontend, step7
    Add view button to toggle additional blog item details

--- part5/bloglist-frontend/src/components/BlogItem.js
+++ part5/bloglist-frontend/src/components/BlogItem.js
@@ -1,8 +1,45 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <li>
-    {blog.title} {blog.author}
-  </li>
-);
+import React, { useState } from 'react';
+
+const Blog = ({ blog, user }) => {
+  const [visible, setVisible] = useState(false);
+
+  const show = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <li className="blog-item">
+      <div>
+        <span>
+          <strong>{blog.title}</strong> by: {blog.author}
+        </span>
+        <div style={show}>
+          <a href="{blog.url}" target="_blank">
+            {blog.url}
+          </a>
+          <div>
+            {blog.likes === 1 ? (
+              <span>
+                <strong>{blog.likes}</strong> Like
+              </span>
+            ) : (
+              <span>
+                <strong>{blog.likes}</strong> Likes
+              </span>
+            )}
+
+            <button className="btn-sm secondary">Like</button>
+          </div>
+          <span>Saved by {blog.user.name}</span>
+        </div>
+      </div>
+      <button className="btn-sm secondary" onClick={toggleVisibility}>
+        View
+      </button>
+    </li>
+  );
+};
 
 export default Blog;
