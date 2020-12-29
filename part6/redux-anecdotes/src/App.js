fab4fcd504907843c4feb0377dad0bdda577f46b
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-29 18:44:55 +1100

    6.5*: anecdotes, step3
    Sort anecdotes by votes desc

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -17,15 +17,19 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+      {anecdotes
+        .sort(function (a, b) {
+          return b.votes - a.votes;
+        })
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div>
