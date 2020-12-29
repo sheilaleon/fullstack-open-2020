b9737fa5b2a2c13194929f5486a30eda49453b7e
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-29 23:48:21 +1100

    6.9 Better anecdotes, step7
    Add Redux DevTools, move Redux-store into its only file

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { vote } from '../reducers/anecdoteReducer';
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => state.anecdote);
   const dispatch = useDispatch();
 
   return (
