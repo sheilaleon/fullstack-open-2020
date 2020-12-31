c495e3f5c3707398d911c0fe0a9683bb90c68eae
Sheila Leon <sheilaleon.au@gmail.com>
2020-12-31 14:32:18 +1100

    7.2: routed anecdotes, step2
    Implement a view for showing a single anecdote

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  useRouteMatch,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
 
 const Menu = () => {
   const padding = {
@@ -25,11 +31,29 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
 );
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>
+        {anecdote.content} by {anecdote.author}
+      </h2>
+      <p>
+        has {anecdote.votes} {anecdote.votes === 1 ? 'vote' : 'votes'}
+      </p>
+      <p>
+        for more info see: <a href={anecdote.info}>{anecdote.info}</a>
+      </p>
+    </div>
+  );
+};
 
 const About = () => (
   <div>
@@ -154,14 +178,22 @@ const App = () => {
     setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
   };
 
+  const match = useRouteMatch('/anecdotes/:id');
+  const anecdote = match
+    ? anecdotes.find((anecdote) => anecdote.id === match.params.id)
+    : null;
+
   return (
-    <Router>
+    <>
       <div>
         <h1>Software anecdotes</h1>
         <Menu />
       </div>
 
       <Switch>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdote={anecdote} />
+        </Route>
         <Route path="/create">
           <CreateNew addNew={addNew} />
         </Route>
@@ -174,7 +206,7 @@ const App = () => {
       </Switch>
 
       <Footer />
-    </Router>
+    </>
   );
 };
 
