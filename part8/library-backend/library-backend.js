ef2b3242229e89464f13db87dc15be01b6ee2895
Sheila Leon <sheilaleon.au@gmail.com>
2021-02-14 17:57:29 +1100

    8.13: Database, part 1 Refactor library app to save to MongoDB

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,88 +1,107 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { v1: uuid } = require('uuid');
+const { ApolloServer, UserInputError, gql } = require('apollo-server');
+// const { v1: uuid } = require('uuid');
+const mongoose = require('mongoose');
+const Author = require('./models/author');
+const Book = require('./models/book');
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-];
+const { MONGODB_URI } = require('./utils/config');
 
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- */
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message);
+  });
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-];
+// let authors = [
+//   {
+//     name: 'Robert Martin',
+//     id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
+//     born: 1952,
+//   },
+//   {
+//     name: 'Martin Fowler',
+//     id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
+//     born: 1963,
+//   },
+//   {
+//     name: 'Fyodor Dostoevsky',
+//     id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
+//     born: 1821,
+//   },
+//   {
+//     name: 'Joshua Kerievsky', // birthyear not known
+//     id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
+//   },
+//   {
+//     name: 'Sandi Metz', // birthyear not known
+//     id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
+//   },
+// ];
+
+// /*
+//  * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
+//  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
+//  */
+
+// let books = [
+//   {
+//     title: 'Clean Code',
+//     published: 2008,
+//     author: 'Robert Martin',
+//     id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
+//     genres: ['refactoring'],
+//   },
+//   {
+//     title: 'Agile software development',
+//     published: 2002,
+//     author: 'Robert Martin',
+//     id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
+//     genres: ['agile', 'patterns', 'design'],
+//   },
+//   {
+//     title: 'Refactoring, edition 2',
+//     published: 2018,
+//     author: 'Martin Fowler',
+//     id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
+//     genres: ['refactoring'],
+//   },
+//   {
+//     title: 'Refactoring to patterns',
+//     published: 2008,
+//     author: 'Joshua Kerievsky',
+//     id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
+//     genres: ['refactoring', 'patterns'],
+//   },
+//   {
+//     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+//     published: 2012,
+//     author: 'Sandi Metz',
+//     id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
+//     genres: ['refactoring', 'design'],
+//   },
+//   {
+//     title: 'Crime and punishment',
+//     published: 1866,
+//     author: 'Fyodor Dostoevsky',
+//     id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
+//     genres: ['classic', 'crime'],
+//   },
+//   {
+//     title: 'The Demon ',
+//     published: 1872,
+//     author: 'Fyodor Dostoevsky',
+//     id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
+//     genres: ['classic', 'revolution'],
+//   },
+// ];
 
 const typeDefs = gql`
   type Author {
@@ -95,9 +114,9 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
-    id: ID!
+    author: Author!
     genres: [String!]!
+    id: ID!
   }
 
   type Query {
@@ -112,8 +131,7 @@ const typeDefs = gql`
       title: String!
       published: Int!
       author: String!
-      id: ID
-      genres: [String]!
+      genres: [String!]!
     ): Book
     editAuthor(name: String!, setBornTo: Int): Author
   }
@@ -143,21 +161,26 @@ const resolvers = {
       books.filter((book) => book.author === root.name).length,
   },
   Mutation: {
-    addBook: (root, args) => {
-      const checkIfAuthorExists = authors.find(
-        (author) => author.name === args.author,
-      );
-      if (!checkIfAuthorExists) {
-        const author = {
-          name: args.author,
-          id: uuid(),
-        };
-        authors = authors.concat(author);
-      } else {
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author });
+
+      if (!author) {
+        author = new Author({ name: args.author });
+
+        try {
+          await author.save();
+        } catch (error) {
+          throw new UserInputError(error, { invalidArgs: args });
+        }
       }
-      const book = { ...args, id: uuid() };
-      books = books.concat(book);
-      console.log('books:>> ', books);
+
+      const book = new Book({ ...args, author: author._id });
+
+      try {
+        await book.save();
+      } catch (error) {
+        throw new UserInputError(error, { invalidArgs: args });
+      }
       return book;
     },
     editAuthor: (root, args) => {
