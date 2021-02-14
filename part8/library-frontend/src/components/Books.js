8cb703866347ac6d2bd40205a9aa1d5e205856c3
Sheila Leon <sheilaleon.au@gmail.com>
2021-02-14 22:35:21 +1100

    8.19 Books by genre, part 1 Implement filter by genre

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = (props) => {
+  const [filter, setFilter] = useState();
+  const [genres, setGenres] = useState([]);
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
+  const { books } = props;
+
+  useEffect(() => {
+    const getGenres = books.flatMap((book) =>
+      book.genres.map((genre) => genre),
+    );
+    setGenres(getGenres);
+  }, []); // eslint-disable-line
+
   if (!props.show) {
     return null;
   }
 
-  const { books } = props;
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+    const booksByGenre = books.filter((book) =>
+      book.genres.includes(event.target.value),
+    );
+    setFilteredBooks(booksByGenre);
+  };
 
   return (
     <div>
       <h2>books</h2>
+      {filteredBooks !== null ? (
+        <p>
+          in genre: <strong>{filter}</strong>
+        </p>
+      ) : null}
+
+      <h3>Filter by:</h3>
+      <div>
+        {genres.map((genre) => (
+          <button
+            key={genre}
+            type="button"
+            value={genre}
+            onClick={handleFilter}
+          >
+            {genre}
+          </button>
+        ))}
+        <button type="button" onClick={() => setFilter('')}>
+          all genres
+        </button>
+        <br />
+      </div>
 
       <table>
         <tbody>
@@ -18,13 +60,21 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {filter !== ''
+            ? filteredBooks.map((a) => (
+                <tr key={a.id}>
+                  <td>{a.title}</td>
+                  <td>{a.author.name}</td>
+                  <td>{a.published}</td>
+                </tr>
+              ))
+            : books.map((a) => (
+                <tr key={a.id}>
+                  <td>{a.title}</td>
+                  <td>{a.author.name}</td>
+                  <td>{a.published}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
