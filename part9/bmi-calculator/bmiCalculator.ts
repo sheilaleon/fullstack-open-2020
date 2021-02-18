8e101528f23d7d1c6d6836ab95ae23cb239b8c32
Sheila Leon <sheilaleon.au@gmail.com>
2021-02-18 21:34:48 +1100

    9.1 Body mass index

---
+++ part9/bmi-calculator/bmiCalculator.ts
@@ -0,0 +1,27 @@
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / Math.pow((height / 100), 2);
+  if (bmi > 40) {
+    return 'Very severely obese';
+  } else if (bmi <= 40 && bmi > 35) {
+    return 'Severely obese';
+  } else if (bmi <= 35 && bmi > 30) {
+    return 'Moderately obese';
+  } else if (bmi <= 30 && bmi > 25) {
+    return 'Overweight';
+  } else if (bmi <= 25 && bmi > 18.5) {
+    return 'Healthy weight';
+  } else if (bmi <= 18.5 && bmi > 16) {
+    return 'Underweight';
+  } else if (bmi <= 16 && bmi > 15) {
+    return 'Severely underweight';
+  } else if (bmi <= 15) {
+    return 'Very severely underweight';
+  };
+  return 'Something went wrong, try again. Use CM for height and KG for weight.'
+};
+
+try {
+  console.log(calculateBmi(180, 74));
+} catch (e) {
+  console.log('Something went wrong, try again. Use CM for height and KG for weight.')
+}
