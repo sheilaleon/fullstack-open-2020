f5aca5783eaf3cf49c573bb858aa82ec8abe8563
Sheila Leon <sheilaleon.au@gmail.com>
2021-02-20 18:40:05 +1100

    9.7 WebExercises

--- part9/bmi-calculator/exerciseCalcuator.ts
+++ part9/bmi-calculator/exerciseCalcuator.ts
@@ -8,37 +8,10 @@ interface ExerciseResults {
   average: number,
 }
 
-interface ExerciseProps {
-  dailyTarget: number
-  weeklyTraining: number[],
-}
+export const calculateExercise = (target: number, daily_exercises: number[], ): ExerciseResults => {
 
-const parseArgs = (args: Array<string>): ExerciseProps => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-
-
-  function getTrainingHours(providedValues: string[]) {
-    if (providedValues.some((value) => isNaN(Number(value)))) {
-      throw new Error('Inputs are not a number, try again.');
-    }
-
-    return providedValues.map((value) => Number(value));
-  }
-    
-  const dailyTarget = Number(args[2]);
-  const providedValues = args.slice(3);
-  const weeklyTraining = getTrainingHours(providedValues);
-
-  return {
-    dailyTarget,
-    weeklyTraining,
-  };
-};
-
-export const calculateExercise = (dailyTarget: number, weeklyTraining: number[], ) : ExerciseResults => {
-
-  const periodLength = weeklyTraining.length;
-  const trainingDays = weeklyTraining.filter((trained) => trained > 0).length;
+  const periodLength = daily_exercises.length;
+  const trainingDays = daily_exercises.filter((trained) => trained > 0).length;
 
   let success = false;
   
@@ -46,13 +19,13 @@ export const calculateExercise = (dailyTarget: number, weeklyTraining: number[],
   let ratingDescription = 'Better luck next time.';
   
   const average: number =
-    weeklyTraining.reduce((acc, cv) => acc + cv, 0) / weeklyTraining.length;
+    daily_exercises.reduce((acc, cv) => acc + cv, 0) / daily_exercises.length;
   
-  if (average >= dailyTarget) {
+  if (average >= target) {
     rating = 3;
     success = true;
     ratingDescription = 'Success~!';
-  } else if (average < dailyTarget && average > 0) {
+  } else if (average < target && average > 0) {
     rating = 2;
     ratingDescription = 'Not too bad, could be better';
   }
@@ -63,14 +36,7 @@ export const calculateExercise = (dailyTarget: number, weeklyTraining: number[],
     success: success,
     rating: rating,
     ratingDescription: ratingDescription,
-    target: dailyTarget,
+    target: target,
     average: average,
   };
 };
-
-try {
-  const { dailyTarget, weeklyTraining } = parseArgs(process.argv);
-  console.log(calculateExercise(dailyTarget, weeklyTraining));
-} catch (error) {
-  console.log(`Something went wrong ---`, error);
-}
--- part9/bmi-calculator/index.ts
+++ part9/bmi-calculator/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 const app = express();
 
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercise } from './exerciseCalcuator';
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -17,6 +20,23 @@ app.get('/bmi', (req, res) => {
   const bmi = calculateBmi(Number(height), Number(weight));
 
   res.send({ height, weight, bmi});
+});
+
+
+app.get('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { target, daily_exercises } = req.body as { target: number, daily_exercises: number[] };
+
+  if (!target || !daily_exercises) {
+    throw new Error('parameters missing');
+  }
+
+  if (isNaN(target) || (daily_exercises.some((value)=> isNaN(Number(value))))) {
+    throw new Error('malformatted parameters');
+  }
+
+  const result = calculateExercise(target, daily_exercises);
+  res.send(result);
 });
 
 const PORT = 3002;
